c6ed66aa16fec543ae10cba5d670d38ca759c1b1
phill <phillip26marchegiany@gmail.com>
2024-11-18 09:34:22 +0700

    login register

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:umkmfirebase/services/auth.dart';
 
 class Register extends StatefulWidget {
@@ -22,7 +23,38 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     final namaUMKMField = TextFormField(
       controller: _namaUMKM,
-      decoration: InputDecoration(hintText: 'Nama UMKM'),
+      decoration: InputDecoration(hintText: 'Nama UMKM',
+        hintStyle: TextStyle(color: Colors.white),
+        labelStyle: TextStyle(color: Colors.white),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+      ),
       autofocus: false,
       validator: (value) {
         if (value!.isEmpty || value == null) {
@@ -30,11 +62,43 @@ class _RegisterState extends State<Register> {
         }
         return null;
       },
+      style: TextStyle(color: Colors.white),
     );
 
     final alamatUMKMField = TextFormField(
       controller: _alamatUMKM,
-      decoration: InputDecoration(hintText: 'Alamat UMKM'),
+      decoration: InputDecoration(hintText: 'Alamat UMKM',
+        hintStyle: TextStyle(color: Colors.white),
+        labelStyle: TextStyle(color: Colors.white),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+      ),
       autofocus: false,
       validator: (value) {
         if (value!.isEmpty || value == null) {
@@ -42,11 +106,43 @@ class _RegisterState extends State<Register> {
         }
         return null;
       },
+      style: TextStyle(color: Colors.white),
     );
 
     final emailField = TextFormField(
       controller: _email,
-      decoration: InputDecoration(hintText: 'Email'),
+      decoration: InputDecoration(hintText: 'Email',
+        hintStyle: TextStyle(color: Colors.white),
+        labelStyle: TextStyle(color: Colors.white),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1,
+          ),
+        ),
+      ),
       autofocus: false,
       validator: (value) {
         if (value!.contains("@") && value!.endsWith(".com")) {
@@ -54,6 +150,7 @@ class _RegisterState extends State<Register> {
         }
         return 'Email tidak sesuai';
       },
+      style: TextStyle(color: Colors.white),
     );
 
     final passwordField = TextFormField(
@@ -61,10 +158,33 @@ class _RegisterState extends State<Register> {
       controller: _password,
       decoration: InputDecoration(
           hintText: 'Password',
+          hintStyle: TextStyle(color: Colors.white),
+          labelStyle: TextStyle(color: Colors.white),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
           suffixIcon: IconButton(
             icon: Icon(
               _showHide ? Icons.visibility_off : Icons.visibility,
-              color: Colors.black,
+              color: Colors.white,
             ),
             onPressed: () {
               setState(() {
@@ -79,18 +199,39 @@ class _RegisterState extends State<Register> {
         }
         return null;
       },
+      style: TextStyle(color: Colors.white),
     );
 
     final txBbutton = TextButton(
-        onPressed: () {
-          widget.toggleview!();
-        },
-        child: Text("Kembali Login"));
+      onPressed: () {
+        widget.toggleview!();
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        // Supaya ukuran Row sesuai dengan kontennya
+        children: [
+          Icon(
+            Icons.arrow_back_ios, // Ikon panah balik
+            color: Colors.white, // Warna ikon
+          ),
+          SizedBox(width: 8), // Jarak antara ikon dan teks
+          Text(
+            "Kembali Login",
+            style: TextStyle(color: Colors.white), // Gaya teks
+          ),
+        ],
+      ),
+    );
+
 
     final loginEmailPasswordButton = Material(
+      borderRadius: BorderRadius.circular(30),
       child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+        // Mengatur padding supaya tombol lebih kompak
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30), // Ujung tombol melengkung
+        ),
         onPressed: () async {
           if (_globalKey.currentState!.validate()) {
             dynamic result = await _authService.registerWithEmailPassword(
@@ -106,49 +247,68 @@ class _RegisterState extends State<Register> {
             }
           }
         },
-        child: Text("Register"),
+        child: Text(
+          "Register",
+          style: TextStyle(
+              fontSize: 16, color: Colors.black), // Gaya teks tombol
+        ),
       ),
     );
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Form(
-            key: _globalKey,
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  namaUMKMField,
-                  SizedBox(
-                    height: 20,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF00A86B), Color(0xFF00A86B)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50.0),
+            // Padding atas dan bawah
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // Atur agar konten di tengah secara horizontal
+              children: [
+                LottieBuilder.asset(
+                  "assets/images/register.json",
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
+                  repeat: true,
+                  reverse: true,
+                ),
+                Form(
+                  key: _globalKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    // Menambahkan padding pada Form
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        namaUMKMField,
+                        SizedBox(height: 20),
+                        alamatUMKMField,
+                        SizedBox(height: 20),
+                        emailField,
+                        SizedBox(height: 20),
+                        passwordField,
+                        SizedBox(height: 20),
+                        loginEmailPasswordButton,
+                        SizedBox(height: 10),
+                        txBbutton,
+                        SizedBox(height: 20),
+                      ],
+                    ),
                   ),
-                  alamatUMKMField,
-                  SizedBox(
-                    height: 20,
-                  ),
-                  emailField,
-                  SizedBox(
-                    height: 20,
-                  ),
-                  passwordField,
-                  SizedBox(
-                    height: 20,
-                  ),
-                  txBbutton,
-                  SizedBox(
-                    height: 20,
-                  ),
-                  loginEmailPasswordButton,
-                ],
-              ),
+                ),
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
