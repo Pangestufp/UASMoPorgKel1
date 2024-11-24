a259ef38bff9dbb5371025ec55c847d92683d1cb
Fedrik Pangestu <fedrikpangestu1@gmail.com>
2024-11-24 17:09:15 +0700

    detailling

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,12 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<FirebaseUser?>.value(
       value: AuthService().user,
       initialData: null,
       child:MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
         home: Wrapper(),
       ),
     );
